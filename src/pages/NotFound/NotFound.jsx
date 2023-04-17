e7296ee428daaad735bf7301cf15def152c078cd
maximedrouault <maximedrouault@gmail.com>
2023-04-17 09:16:15 +0200

    Intégration de la page "NotFound".

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,15 @@
+import "./notfound.css"
+import { Link } from "react-router-dom"
 import App from "../../App"
 
 export default function NotFound() {
     return (
         <App>
-            <h1>Not Found "404"</h1>
+            <section className="section-notfound">
+                <h1 className="not-found-title">404</h1>
+                <h2 className="not-found-subtitle">Oups! La page que vous demandez n'existe pas.</h2>
+                <Link to="/" className="link-return-home">Retourner sur la page d’accueil</Link>
+            </section>
         </App>
     )
 }
