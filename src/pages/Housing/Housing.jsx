bbb43ccd426864a900bc1e11a801f8250dec1245
maximedrouault <maximedrouault@gmail.com>
2023-04-21 12:28:05 +0200

    Intégration de la gestion de l'erreur sur ID d'un logement et améliorations du code.

--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,6 +1,6 @@
 import "./housing.css";
-import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useParams, useNavigate  } from "react-router-dom";
+import { useState, useEffect } from "react";
 import App from "../../App";
 import Carousel from "../../components/Carousel/Carousel";
 import FetchDatas from "../../services";
@@ -10,35 +10,60 @@ import HousingHostInfos from "../../components/HousingHostInfos/HousingHostInfos
 import HousingRate from "../../components/HousingRate/HousingRate";
 
 export default function Housing() {
-  
-  // Import des données
-  const [housings] = useState(FetchDatas());
 
   // Utilisation de useParams pour récupérer l'id dans l'URL
   const { id } = useParams();
+  // Intialisation du State "housingId"
+  const [housingId, setHousingId] = useState(null);
+  // Initialisation de useNavigate pour rediriger vers la page NotFound si l'ID n'existe pas
+  const navigate = useNavigate();
 
-  // Trouver la maison dans le tableau des logements en fonction de l'id
-  const [housing] = useState(housings.find((housing) => housing.id === id));
 
+  useEffect(() => {
+
+    // Définition de la fonction permettant la sélection du logement via l'ID
+    async function selectHousing() {
+      // Récupération des données
+      const housings = await FetchDatas();
+      // Recherche du logement via l'ID récupéré dans l'URL par useParam
+      const selectedHousing = housings.find((housing) => housing.id === id);
+      // Mise à jour du State "HOUSING" avec l'id récupéré
+      setHousingId(selectedHousing);
+      // Redirection vers la pages "NotFound" si l'ID est inexistant
+      if (!selectedHousing) {
+        navigate("/notfound");
+      }
+    }
+
+    selectHousing();
+
+  }, [id, navigate]);
+
+  // Si le state "housingId" est null ou undefined, renvoi vers la page NotFound
+  if (!housingId) {
+    return null;
+  }
+
+  // Sinon, génération de la page Housing
   return (
     <App>
-      <Carousel pictures={housing.pictures} />
+      <Carousel pictures={housingId.pictures} />
       <section>
         <div className="housing-container">
-          <HousingInfos housing={housing} />
+          <HousingInfos housing={housingId} />
           <div className="housing-subcontainer-right">
-            <HousingHostInfos housing={housing} />
-            <HousingRate rating={housing.rating} />
+            <HousingHostInfos housing={housingId} />
+            <HousingRate rating={housingId.rating} />
           </div>
         </div>
 
         <div className="housing-informations-container">
           <Collapse title={"Description"}>
-            <p>{housing.description}</p>
+            <p>{housingId.description}</p>
           </Collapse>
           <Collapse title={"Équipements"}>
             <ul>
-              {housing.equipments.map((equipment, index) => (
+              {housingId.equipments.map((equipment, index) => (
                 <li key={index}>{equipment}</li>
               ))}
             </ul>
